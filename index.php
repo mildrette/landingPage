f319109d367f0e57e68ebb419a2af371d61677ea
mildrette <mildredfonka1@gmail.com>
2025-04-12 22:27:03 +0100

    added code in the php html syntax

--- index.php
+++ index.php
@@ -1,7 +1,21 @@
 <!DOCTYPE html>
 <html>
-<head><title>deku</title></head>
+<head>
+  <title>Learning PHP</title>
+</head>
+
 <body>
-  <h1><?php echo "Hello from PHP!"; ?></h1>
+  <h1>Welcome to PHP</h1>
+  <p>
+    <?php 
+      echo "I am learning PHP for my first time on " . date("l, F jS") . ", and the goal is to build a website in PHP.";  
+    ?>
+  </p>
+
+  <p>
+    <?php
+    echo "Learning PhP for the first time on the" . date("l, F jS") . ", so what do your think right now??.";
+    ?>
+      </p>
 </body>
 </html>
